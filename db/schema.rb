665ea572bbb2dec0784748a720eb274fb00fe8bb
Katrina Pritchard <pritckat@gmail.com>
2018-09-14 20:19:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914185850) do
+ActiveRecord::Schema.define(version: 20180914200010) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string  "name"
-    t.integer "song_id"
+    t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string  "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
+    t.string "name"
   end
 
 end
