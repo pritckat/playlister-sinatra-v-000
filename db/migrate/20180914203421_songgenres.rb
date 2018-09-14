b024ae6e81d3f8c647a1c2c3d8e9bfab9020f8d4
Katrina Pritchard <pritckat@gmail.com>
2018-09-14 20:38:48 +0000

    automatically backed up by learn

--- db/migrate/20180914203421_songgenres.rb
+++ db/migrate/20180914203421_songgenres.rb
@@ -1,7 +1,7 @@
 class Songgenres < ActiveRecord::Migration
   def change
     create_table :song_genre do |t|
-      t.integer :artist_id 
+      t.integer :artist_id
       t.integer :genre_id
     end
   end
